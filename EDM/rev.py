9eaa6a3a060875c4a1f3007ed5eedd43a1782984
Konstantin Navrockiy <nkp2169@i.ua>
2014-01-02 22:21:03 +0200

    Update rev.py

--- EDM/rev.py
+++ EDM/rev.py
@@ -3,28 +3,16 @@ import hal, linuxcnc
 h = hal.component("rev")
 h.newpin("stop", hal.HAL_BIT, hal.HAL_IN)
 h.newpin("rev", hal.HAL_BIT, hal.HAL_IN)
-h.newpin("rerev", hal.HAL_BIT, hal.HAL_IN)
+h.newpin("rerev-stop", hal.HAL_BIT, hal.HAL_IN)
 h.newpin("curent-line",hal.HAL_S32,hal.HAL_IN)
 h.newpin("minmax-reset", hal.HAL_BIT, hal.HAL_OUT) 
-
-
+h.newpin("again_forward", hal.HAL_BIT, hal.HAL_IN)
 h.newpin("number-in",hal.HAL_S32,hal.HAL_IN)
 h.newpin("number-out", hal.HAL_S32, hal.HAL_OUT) 
     
 h.ready()
 c = linuxcnc.command()
 s = linuxcnc.stat()
-def ensure_mode(m, *p):
-    s.poll()
-    if s.task_mode == m or s.task_mode in p: return True
-    if running(do_poll=False): return False
-    c.mode(m)
-    c.wait_complete()
-    return True
-    
-def running(do_poll=True):
-    if do_poll: s.poll()
-    return s.task_mode == linuxcnc.MODE_AUTO and s.interp_state != linuxcnc.INTERP_IDLE
 def ret_line(number):
 	f = open('/home/nkp/temp_rev.ngc', "r") #here prepared code(revers Gcode)
 	lines = f.readlines()
@@ -33,13 +21,13 @@ def ret_line(number):
 	    print number
 	    return lines[number]
 	else:
-	    return lines[0]
-
-		   
+	    return lines[0]		   
 def stop():
 	c.abort()
 	c.wait_complete()
+	print 'ok-stop'
  	h["stop"]=0
+ 	h["rerev-stop"]=0
 def back():
 	c.mode(linuxcnc.MODE_MDI)
 	c.wait_complete()
@@ -47,32 +35,31 @@ def back():
 	c.wait_complete()
 	h["minmax-reset"] = 0
 	h["rev"]=0
-def again_forward():
-	
-	c.abort()
-	c.wait_complete()
-#	s.poll()
-	'''if s.interp_state == linuxcnc.INTERP_IDLE:
-		print c.wait_complete(),':c.wait_complete()'
-		ensure_mode(linuxcnc.MODE_AUTO)
-		if ensure_mode(linuxcnc.MODE_AUTO) :
-			c.auto(linuxcnc.AUTO_RUN, (h["curent-line"]+1))
-		else:
-			print 'linuxcnc.MODE_AUTO False'
-		h["minmax-reset"] = 1
-		h["rerev"]=0
-		print 'ok'
+def again_forward():	
+        s.poll()
+	if s.interp_state == linuxcnc.INTERP_IDLE:
+                c.mode(linuxcnc.MODE_AUTO)
+                c.wait_complete()
+                s.poll()
+                c.wait_complete()
+                if s.task_mode == 2 :
+                        c.auto(linuxcnc.AUTO_RUN, (h["curent-line"]+1))
+                        h["minmax-reset"] = 1
+                else:
+                        print 'linuxcnc.MODE_AUTO False'                
+                h["again_forward"]=0
+                print 'ok'
 	else:
-		h["rerev"]=0
-		return'''
-	h["rerev"]=0
+                h["again_forward"]=0	
 try:
     while 1:	 
 	if h["stop"]==1:
+		stop()		
+	if h["rerev-stop"]==1:
 		stop()
 	if h["rev"]==1:
-		back()		
-	if h["rerev"]==1:
-		again_forward()	
+		back()
+	if h["again_forward"]==1:
+		again_forward()			
 except KeyboardInterrupt:
     raise SystemExit
